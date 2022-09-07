ec506686a033d8e36ba05c4c865ffc2840ea33d2
ghazalehshahin <shahinghazaleh@gmail.com>
2022-09-07 16:35:28 +0430

    inspecting image's values

--- CV1.py
+++ CV1.py
@@ -1,5 +1,18 @@
 import cv2 as cv
-img = cv.imread ('image.jpg')
-cv.imshow("image", img)
-cv.waitKey(0)
-cv.destroyAllWindows()
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+img = cv.imread ('dolphin.png')
+##show image and wait
+# cv.imshow("image", img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+##check for availability
+if img is None:
+    sys.exit("Could not read the image.")
+##size and shape of image
+# img.shape
+##get data from a special part of picture
+for i in range (101, 104):
+    for j in range (201, 204):
+       print(img[i,j])
