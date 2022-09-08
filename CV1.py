d388240b5b01c6fe1a3d08e08d28925fb950ef78
ghazalehshahin <shahinghazaleh@gmail.com>
2022-09-08 10:47:35 +0430

    cropping an image

--- CV1.py
+++ CV1.py
@@ -2,17 +2,34 @@ import cv2 as cv
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-img = cv.imread ('dolphin.png')
+img = cv.imread ('bicycle.png')
+
+
 ##show image and wait
 # cv.imshow("image", img)
 # cv.waitKey(0)
 # cv.destroyAllWindows()
+
+
 ##check for availability
 if img is None:
     sys.exit("Could not read the image.")
+
+
 ##size and shape of image
 # img.shape
+
+
 ##get data from a special part of picture
-for i in range (101, 104):
-    for j in range (201, 204):
-       print(img[i,j])
+# for i in range (101, 104):
+#     for j in range (201, 204):
+#        print(img[i,j])
+
+##cropping an image
+# print(img.shape)
+# cv.imshow("original", img)
+cropped_img = img[110:310, 10:160]
+print(cropped_img.shape)
+cv.imshow("cropped", cropped_img)
+cv.waitKey(0)
+cv.destroyAllWindows()
